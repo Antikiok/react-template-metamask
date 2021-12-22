df7696a3fc53e804e47b171becf2d64244e951b2
Antikiok <antikiok@gmail.com>
2021-12-22 12:35:46 +0200

    adding send fuctional

--- src/components/SendCrypto/SendCrypto.js
+++ src/components/SendCrypto/SendCrypto.js
@@ -1,54 +1,71 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
-const SendCrypto = () => (
-  <form className="event-form">
-    <input
-      type="text"
-      // value={title}
-      name="title"
-      placeholder="Title"
-      className="event-form__field"
-      //   onChange={}
-    />
-    <div className="event-form__time">
+const SendCrypto = ({ account, ethereum }) => {
+  const [eventFormData, setEventFormData] = useState({
+    wallet: '',
+    amount: 0,
+  });
+  const { wallet, amount } = eventFormData;
+
+  // Don't understand how i can convert to heximal value
+  // and send this value in function sendEth
+  const amountHex16 = amount.toString(16);
+
+  const handleEventForm = (e) => {
+    const { name, value } = e.target;
+    setEventFormData({
+      ...eventFormData,
+      [name]: value,
+    });
+  };
+
+  const sendEth = (e) => {
+    e.preventDefault();
+    ethereum
+      .request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: account,
+            to: wallet,
+            value: amountHex16,
+            gasPrice: '0x09184e72a000',
+            gas: '0x2710',
+          },
+        ],
+      })
+      .then((txHash) => console.log(txHash))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <form className="event-form">
       <input
-        type="date"
-        //   value={date}
-        name="date"
+        type="text"
+        value={wallet}
+        name="wallet"
+        placeholder="Input wallet which you need to send"
         className="event-form__field"
-        // onChange={}
+        onChange={handleEventForm}
       />
       <input
-        type="time"
-        //   value={startTime}
-        name="startTime"
-        className="event-form__field"
-        // onChange={}
+        type="number"
+        value={amount}
+        name="amount"
+        placeholder="Input wallet which you need to send"
+        className="event-form__amount"
+        onChange={handleEventForm}
       />
-      <span>-</span>
-      <input
-        type="time"
-        //   value={endTime}
-        name="endTime"
-        className="event-form__field"
-        // onChange={}
-      />
-    </div>
-    <textarea
-      name="description"
-      // value={description}
-      placeholder="Description"
-      className="event-form__field"
-      //   onChange={}
-    ></textarea>
-    <button
-      type="submit"
-      className="event-form__submit-btn"
-      // onClick={toggleModal}
-    >
-      Submit Create
-    </button>
-  </form>
-);
+      <button
+        type="submit"
+        className="event-form__submit-btn"
+        onClick={sendEth}
+      >
+        Send
+      </button>
+    </form>
+  );
+};
 
 export default SendCrypto;
